6126e02e958bc87d557ab5693af23703585028a3
OGFRANKOFEAST <132195774+OGFRANKOFEAST@users.noreply.github.com>
2024-02-02 12:46:47 +0630

    first commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -42,16 +42,14 @@ const Hero = () => {
             </div>
             <div className="flex flex-col w-full ga-3 sm:flex-row">
             <Button 
-            type="button" 
-            title="Download App" 
-            variant="btn_green" 
-          />
+            type="button"
+            title="Download App"
+            variant="btn_green" full={false}          />
            <Button 
-            type="button" 
-            title="How we work?" 
+            type="button"
+            title="How we work?"
             icon="/play.svg"
-            variant="btn_white_text" 
-          />
+            variant="btn_white_text" full={false}          />
             </div>
 
 
